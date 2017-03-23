a453f4a9d40bd329767a44de3a400528a2d5e317
shule1987 <shule1987@qq.com>
2017-03-23 08:09:42 +0800

    create an our own cursor theme
    Change-Id: If93bf42c46b4d4a3146053d6ff9f49a611f8ac83

---
+++ cursors-src/cursors/bitmaps/make.sh
@@ -0,0 +1,104 @@
+#!/bin/sh
+#xcursorgen based_arrow_down.in ../../../deepin/cursors/based_arrow_down
+#xcursorgen based_arrow_up.in ../../../deepin/cursors/based_arrow_up
+#xcursorgen center_ptr.in ../../../deepin/cursors/center_ptr
+#xcursorgen hand1.in ../../../deepin/cursors/hand1
+xcursorgen all-scroll.in ../../../deepin/cursors/all-scroll
+xcursorgen bd_double_arrow.in ../../../deepin/cursors/bd_double_arrow
+xcursorgen bottom_left_corner.in ../../../deepin/cursors/bottom_left_corner
+xcursorgen bottom_right_corner.in ../../../deepin/cursors/bottom_right_corner
+xcursorgen bottom_side.in ../../../deepin/cursors/bottom_side
+xcursorgen bottom_tee.in ../../../deepin/cursors/bottom_tee
+xcursorgen circle.in ../../../deepin/cursors/circle
+xcursorgen copy.in ../../../deepin/cursors/copy
+xcursorgen cross.in ../../../deepin/cursors/cross
+xcursorgen crossed_circle.in ../../../deepin/cursors/crossed_circle
+xcursorgen crosshair.in ../../../deepin/cursors/crosshair
+xcursorgen dnd-ask.in ../../../deepin/cursors/dnd-ask
+xcursorgen dnd-copy.in ../../../deepin/cursors/dnd-copy
+xcursorgen dnd-link.in ../../../deepin/cursors/dnd-link
+xcursorgen dnd-move.in ../../../deepin/cursors/dnd-move
+xcursorgen dnd-none.in ../../../deepin/cursors/dnd-none
+xcursorgen dotbox.in ../../../deepin/cursors/dotbox
+xcursorgen fd_double_arrow.in ../../../deepin/cursors/fd_double_arrow
+xcursorgen grabbing.in ../../../deepin/cursors/grabbing
+xcursorgen hand2.in ../../../deepin/cursors/hand2
+xcursorgen left_ptr.in ../../../deepin/cursors/left_ptr
+xcursorgen left_ptr_watch.in ../../../deepin/cursors/left_ptr_watch
+xcursorgen left_side.in ../../../deepin/cursors/left_side
+xcursorgen left_tee.in ../../../deepin/cursors/left_tee
+xcursorgen link.in ../../../deepin/cursors/link
+xcursorgen ll_angle.in ../../../deepin/cursors/ll_angle
+xcursorgen lr_angle.in ../../../deepin/cursors/lr_angle
+xcursorgen move.in ../../../deepin/cursors/move
+xcursorgen pencil.in ../../../deepin/cursors/pencil
+xcursorgen plus.in ../../../deepin/cursors/plus
+xcursorgen pointer-move.in ../../../deepin/cursors/pointer-move
+xcursorgen question_arrow.in ../../../deepin/cursors/question_arrow
+xcursorgen right_ptr.in ../../../deepin/cursors/right_ptr
+xcursorgen right_side.in ../../../deepin/cursors/right_side
+xcursorgen right_tee.in ../../../deepin/cursors/right_tee
+xcursorgen sb_down_arrow.in ../../../deepin/cursors/sb_down_arrow
+xcursorgen sb_h_double_arrow.in ../../../deepin/cursors/sb_h_double_arrow
+xcursorgen sb_left_arrow.in ../../../deepin/cursors/sb_left_arrow
+xcursorgen sb_right_arrow.in ../../../deepin/cursors/sb_right_arrow
+xcursorgen sb_up_arrow.in ../../../deepin/cursors/sb_up_arrow
+xcursorgen sb_v_double_arrow.in ../../../deepin/cursors/sb_v_double_arrow
+xcursorgen tcross.in ../../../deepin/cursors/tcross
+xcursorgen top_left_corner.in ../../../deepin/cursors/top_left_corner
+xcursorgen top_right_corner.in ../../../deepin/cursors/top_right_corner
+xcursorgen top_side.in ../../../deepin/cursors/top_side
+xcursorgen top_tee.in ../../../deepin/cursors/top_tee
+xcursorgen ul_angle.in ../../../deepin/cursors/ul_angle
+xcursorgen ur_angle.in ../../../deepin/cursors/ur_angle
+xcursorgen vertical-text.in ../../../deepin/cursors/vertical-text
+xcursorgen watch.in ../../../deepin/cursors/watch
+xcursorgen X_cursor.in ../../../deepin/cursors/X_cursor
+xcursorgen xterm.in ../../../deepin/cursors/xterm
+xcursorgen zoom-in.in ../../../deepin/cursors/zoom-in
+xcursorgen zoom-out.in ../../../deepin/cursors/zoom-out
+
+cd ../../../deepin/cursors
+ln -sf    dotbox			draped_box
+ln -sf    dotbox			icon
+ln -sf    dotbox			target
+ln -sf    dotbox			dot_box_mask
+ln -sf    X_cursor			pirate
+ln -sf    left_ptr_watch	08e8e1c95fe2fc01f976f1e063a24ccd
+ln -sf    left_ptr_watch	3ecb610c1bf2410f44200f48c40d3599
+ln -sf    left_ptr			arrow
+ln -sf    left_ptr			top_left_arrow
+ln -sf    right_ptr			draft_large
+ln -sf    right_ptr			draft_small
+ln -sf    move				4498f0e0c1937ffe01fd06f973665830
+ln -sf    move				9081237383d90e509aa00f00170e968f
+ln -sf    copy				1081e37283d90000800003c07f3ef6bf
+ln -sf    copy				6407b0e94181790501fd1e167b474872
+ln -sf    cross				cross_reverse
+ln -sf    cross				diamond_cross
+ln -sf    hand2				9d800788f1b08800ae810202380a0822
+ln -sf    hand2				e29285e634086352946a0e7090d73106
+ln -sf    hand2				hand
+ln -sf    hand2				hand1
+ln -sf    grabbing			fleur
+ln -sf    question_arrow	d9ce0ab605698f320427677b458ad60b
+ln -sf    question_arrow	5c6cd98b3f3ebcb1f9c7f1c204630408
+ln -sf    question_arrow	help
+ln -sf    question_arrow	left_ptr_help
+ln -sf    link				3085a0e285430894940527032f8b26df
+ln -sf    link				640fb0e74195791501fd1ed57b41487f
+ln -sf    crossed_circle	03b6e0fcb3499374a867c041f52298f0
+ln -sf    fd_double_arrow	fcf1c3c7cd4491d801f1e1c78f100000
+ln -sf    bd_double_arrow	c7088f0f3e6c8088236ef8e1e3e70000
+#ln -sf    based_arrow_up	base_arrow_up
+#ln -sf    based_arrow_down	base_arrow_down
+ln -sf    sb_h_double_arrow h_double_arrow
+ln -sf    sb_h_double_arrow	14fef782d02440884392942c11205230
+ln -sf    h_double_arrow	028006030e0e7ebffc7f7070c0600140
+ln -sf    sb_v_double_arrow double_arrow
+ln -sf    sb_v_double_arrow v_double_arrow
+ln -sf    sb_v_double_arrow	2870a09082c103050810ffdffffe0204
+ln -sf    v_double_arrow	00008160000006810000408080010102
+#ln -sf    center_ptr		centre_ptr
+
+#cp -RPv * /usr/share/icons/deepin/cursors/
